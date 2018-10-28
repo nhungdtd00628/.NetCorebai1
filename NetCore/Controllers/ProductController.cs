3b6d46d74d604c47cb4da718ff75941dabd8b3d6
= <=>
2018-10-28 22:32:37 +0700

    first commit

---
+++ NetCore/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NetCore.Models;
+
+namespace NetCore.Controllers
+{
+    public class ProductController : Controller
+    {
+        [Route("")]
+        public IActionResult GetList()
+        {
+            return View();
+        }
+
+        public IActionResult Create(string name, int price)
+        {
+            ViewData["name"] = name;
+            ViewData["price"] = price;
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+        public IActionResult Delete(int ID)
+        {
+            return new JsonResult(new Product()
+            {
+                id = ID
+
+            });
+        }
+    }
+}
